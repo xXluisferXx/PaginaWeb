c0da961df2d6108ee052d430f5f99f352b1dd578
xXluisferXx <luisfernandogalindoquintero@gmail.com>
2023-05-22 15:03:40 -0500

    repair links and add my.js

---
+++ js/my.js
@@ -0,0 +1,17 @@
+let enlaces_header = document.querySelectorAll(".enlaces-header")[0];
+let icon_hamburguer = true;
+ document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
+    if(icon_hamburguer){
+        document.querySelectorAll(".hamburguer")[0].style.color="#fff";
+        icon_hamburguer = false;
+    }
+    else{
+        document.querySelectorAll(".hamburguer")[0].style.color="#000";
+        icon_hamburguer = true;
+    }
+    enlaces_header.classList.toggle("hamburgueropen");
+})
+ document.querySelectorAll(".enlaces-header")[0].addEventListener("click", function(){
+    enlaces_header.classList.toggle("hamburgueropen");
+    document.querySelectorAll(".hamburguer")[0].style.color ="#000"
+})
